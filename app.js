5b68f3b2cd3ef621ea2bbdb5843d9ac61029cb39
Gennaro <genzr05@gmail.com>
2020-04-13 20:26:28 +1000

    added comments to code

--- app.js
+++ app.js
@@ -2,13 +2,18 @@ const express = require ('express')
 const port = 5000
 const app = express()
 
+//* Middleware *//
+// Allows use of request body data
 app.use(express.json())
+// Provides additional terminal logging 
 app.use(require('morgan')('dev'))
 
+// Basic response handler for GET request on / route
 app.get('/', (req,res) => {
     res.send('Hello World')
 })
 
+// Sets up listener on port 5000
 app.listen(port, () => {
     console.log('Express server listening on port ' + port)
 })
